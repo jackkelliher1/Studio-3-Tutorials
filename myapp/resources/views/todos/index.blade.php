afa4d767664131454226d782f7c884f86aea9933
jackkelliher1 <55121775+jackkelliher1@users.noreply.github.com>
2020-08-24 10:40:16 +1200

    finished

--- myapp/resources/views/todos/index.blade.php
+++ myapp/resources/views/todos/index.blade.php
@@ -6,8 +6,7 @@
             <li class="list-group-item my-2">
                 <h5 class="float-left">{{$todo->title}}</h5>
                 <div class="clearfix"></div>
-                <p>{{ \Illuminate\Support\Str::limit($todo, 500, '...') }}</p>
-                <small class="float-right">{{$todo->created_at->diffForHumans()}}</small>
+              
                 <a href="{{route('todos.show',$todo->id)}}">Read More</a>
             </li>
         @empty
